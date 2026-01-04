63201d77acb3086593b3871a1246997814ae0fa3
navbug <naveenbugalia512@gmail.com>
2026-01-04 17:20:37 +0530

    removed mongodb uri from code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WebinarsModule } from './webinars/webinars.module';
 import { AttendeesModule } from './attendees/attendees.module';
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://naveen:naveen@cluster0.k7a72lu.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(uri),
+    MongooseModule.forRoot(uri!),
     WebinarsModule,
     AttendeesModule,
   ],
